72567b01e165bac23c005950f4a4dc8af5c728a1
jeonghun <munbuk2@hanmail.net>
2022-04-24 21:10:11 +0900

    220424 문정훈
    메인 관리자 페이지 작업중 (DB 테이블생성, JSP파일 생성, css 내용 제작중)

---
+++ KKMSProj/src/pack_Maincon/MainContentsBean.java
@@ -0,0 +1,45 @@
+package pack_Maincon;
+
+public class MainContentsBean {
+	private int mNo;        // 번호
+	private String mTitle;  // 전시회 제목
+	private String mImg;   // 파일이름
+	private String mDate1; // 전시회 시작날짜
+	private String mDate2; // 전시회 종료날짜
+	
+	
+	
+	
+	public int getmNo() {
+		return mNo;
+	}
+	public void setmNo(int mNo) {
+		this.mNo = mNo;
+	}
+	public String getmTitle() {
+		return mTitle;
+	}
+	public void setmTitle(String mTitle) {
+		this.mTitle = mTitle;
+	}
+	public String getmImg() {
+		return mImg;
+	}
+	public void setmImg(String mImg) {
+		this.mImg = mImg;
+	}
+	public String getmDate1() {
+		return mDate1;
+	}
+	public void setmDate1(String mDate1) {
+		this.mDate1 = mDate1;
+	}
+	public String getmDate2() {
+		return mDate2;
+	}
+	public void setmDate2(String mDate2) {
+		this.mDate2 = mDate2;
+	}
+	
+	
+}
---
+++ KKMSProj/src/pack_Maincon/MainconMgr.java
@@ -0,0 +1,81 @@
+package pack_Maincon;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.List;
+import java.util.Vector;
+
+import pack_DBCP.DBConnectionMgr;
+
+public class MainconMgr {
+	
+	private DBConnectionMgr pool = null;
+	private Connection objConn = null;
+	private PreparedStatement objPstmt = null;
+	private ResultSet objRS = null;
+	private String sql = "";
+	
+	public MainconMgr() {
+		try {
+			pool = DBConnectionMgr.getInstance();
+		} catch (Exception e) {
+			e.printStackTrace(); 
+
+		}
+	}
+	
+	// 데이터 출력용
+	public List<MainContentsBean> ListOutput(){
+		
+		List<MainContentsBean> mList = new Vector<MainContentsBean>();
+		
+		try {
+			objConn = pool.getConnection();
+			sql = "select * from proList order by pNo Asc";
+			objPstmt = objConn.prepareStatement(sql);
+			objRS = objPstmt.executeQuery();
+			while(objRS.next()) {
+				MainContentsBean mCBean = new MainContentsBean();
+				mCBean.setmNo(objRS.getInt("mNo"));
+				mCBean.setmTitle("mTitle");
+				mCBean.setmImg("mImg");
+				mCBean.setmDate1("mDate1");
+				mCBean.setmDate1("mDate2");
+				mList.add(mCBean);
+				
+				
+			}
+		}catch (Exception e) {
+			System.out.println("listOutput e : " + e.getMessage());
+		} finally {
+			pool.freeConnection(objConn, objPstmt, objRS);
+		}
+		
+		return mList;
+	}
+	
+	public int mListCount() {
+		int count = 0;
+		
+		try {
+			objConn = pool.getConnection();
+			sql = "select * from proList order by pNo Asc";
+			objPstmt = objConn.prepareStatement(sql);
+			objRS = objPstmt.executeQuery();
+			
+			if(objRS.next()) {
+				count = objRS.getInt(1);
+			}
+		}catch(Exception e) {
+			System.out.println("mListCount e : "+ e.getMessage());
+		}finally {
+			pool.freeConnection(objConn, objPstmt, objRS);
+		}
+		return count;
+	}
+	
+
+
+
+}
